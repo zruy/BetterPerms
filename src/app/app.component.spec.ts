57f6c84ef00e8293956a5a95b1f42688f8801fe0
zruy <109708027+zruy@users.noreply.github.com>
2021-02-28 16:35:38 +0000

    Update markdown.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,4 +12,4 @@ describe('AppComponent', () => {
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createCompon
+    const fixture = TestBed.createComponent(AppCompon
