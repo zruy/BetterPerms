63b72eeecf974d8eecd9414fbce4f84c196c439c
zruy <109708027+zruy@users.noreply.github.com>
2021-03-04 18:42:11 +0000

    tooltip

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,4 +13,5 @@ describe('AppComponent', () => {
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componen
+    const app = fixture.debugElement.componentInstance;
+    ex
