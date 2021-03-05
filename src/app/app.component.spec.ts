2db4da2e4f2c52c6d2ab848bd017b8156c45a770
zruy <109708027+zruy@users.noreply.github.com>
2021-03-05 19:10:07 +0000

    Bump version to 0.9.4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,4 +14,5 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).to
+    expect(app).toBeTruthy();
+  }))
