5c1f0317446dddc2a67b42c52adf08017a9c8aff
zruy <109708027+zruy@users.noreply.github.com>
2021-03-06 07:02:51 +0000

    Update: manage-your-web-site-images-in-the-cloud.md

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,4 +15,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }))
+  }));
+
+  it(`sho
