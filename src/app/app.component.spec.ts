e0077ff20cace589c456de9a1fb1dac07b0ab643
zruy <109708027+zruy@users.noreply.github.com>
2021-03-18 06:51:38 +0000

    folders

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,4 +20,7 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqu
+    expect(app.title).toEqual('app');
+  }));
+
+ 
