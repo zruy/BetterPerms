b620bf2158fe84b560a9741ea0ef362e2e77b98f
zruy <109708027+zruy@users.noreply.github.com>
2021-03-20 15:42:08 +0000

    adds example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,4 +24,5 @@ describe('AppComponent', () => {
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppCompone
+    const fixture = TestBed.createComponent(AppComponent);
+    fixtu
