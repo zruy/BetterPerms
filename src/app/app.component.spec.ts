dbc63c7bafaa69bfa882bbce7a18892d5bbfc84a
zruy <109708027+zruy@users.noreply.github.com>
2021-03-29 18:51:54 +0000

    Update smike.py

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,4 +27,4 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector
+    expect(compiled.querySelector('h1').textCont
