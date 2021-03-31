bd716e6592e5c817741205ad2680e72073792933
zruy <109708027+zruy@users.noreply.github.com>
2021-03-31 07:12:38 +0000

    Update and rename README.md to README.rst

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,4 +27,4 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textCont
+    expect(compiled.querySelector('h1').textContent).toContai
