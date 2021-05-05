56c99315560c12513661144e7217e7cf26596cc1
zruy <109708027+zruy@users.noreply.github.com>
2021-05-05 09:23:09 +0000

    Updated makefile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {CommonModule} from '@angu
+import {CommonModule} from '@angular/common
