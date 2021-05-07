9aefddf05fdce123e9db5fafa82e8e6834695d99
zruy <109708027+zruy@users.noreply.github.com>
2021-05-07 07:45:27 +0000

    Remove debug trace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,4 +2,4 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CommonModule} from '@angular/common';
-import {H
+import {HeroModule} fro
