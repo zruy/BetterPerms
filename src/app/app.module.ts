dc8e9da661acb027ebd5b6d6ba3c4b96d45b5777
zruy <109708027+zruy@users.noreply.github.com>
2021-05-14 21:52:23 +0000

    Update Darwinism

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,4 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CommonModule} from '@angular/common';
-import {HeroModule} from './modules/hero/hero.m
+import {HeroModule} from './modules/hero/hero.module';
+
+import {A
