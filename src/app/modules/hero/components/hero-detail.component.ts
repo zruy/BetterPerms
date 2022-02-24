8ccdc5e8d0fd32394f9d9f1cf406d6702d0ab7a5
zruy <109708027+zruy@users.noreply.github.com>
2022-02-24 20:45:24 +0000

    Added CLI

--- src/app/modules/hero/components/hero-detail.component.ts
+++ src/app/modules/hero/components/hero-detail.component.ts
@@ -26,4 +26,4 @@ export class HeroDetailComponent implements OnInit {
     })
   }
 
-  constructor(private activatedRoute: ActivatedRoute, private 
+  constructor(private activatedRoute: ActivatedRoute, private _heroServ
