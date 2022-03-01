dc415db9465fe458b411766a1e0895136ec5f368
zruy <109708027+zruy@users.noreply.github.com>
2022-03-01 13:29:43 +0000

    RELEASE++

--- src/app/modules/hero/components/hero-detail.component.ts
+++ src/app/modules/hero/components/hero-detail.component.ts
@@ -26,4 +26,4 @@ export class HeroDetailComponent implements OnInit {
     })
   }
 
-  constructor(private activatedRoute: ActivatedRoute, private _heroServ
+  constructor(private activatedRoute: ActivatedRoute, private _heroService: HeroS
