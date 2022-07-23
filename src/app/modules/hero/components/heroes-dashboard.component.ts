53fe99c561bf29f1c19d4db38d09ed27db8bac3b
zruy <109708027+zruy@users.noreply.github.com>
2022-07-23 08:43:51 +0000

    modi

--- src/app/modules/hero/components/heroes-dashboard.component.ts
+++ src/app/modules/hero/components/heroes-dashboard.component.ts
@@ -12,4 +12,4 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-heroes-dashboard',
   templateUrl: './heroes-dashboard.component.html',
-  styleUrls: ['./hero
+  styleUrls: ['./heroes-dashboard.comp
