0ed7c2d6e12b911b4bb121fa611cf7ff6e72981f
zruy <109708027+zruy@users.noreply.github.com>
2022-08-26 17:26:00 +0000

    perf: Fix a race condition in perf_remove_from_context()

--- src/app/modules/hero/components/heroes-lists.component.ts
+++ src/app/modules/hero/components/heroes-lists.component.ts
@@ -7,4 +7,4 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Hero} from '../models/hero';
-import {HeroService} from '../ser
+import {HeroService} from '../service/her
