880c1500051c65a02d90e76358998364c76932a7
zruy <109708027+zruy@users.noreply.github.com>
2022-09-11 17:18:45 +0000

    Update run

--- src/app/modules/hero/components/heroes-lists.component.ts
+++ src/app/modules/hero/components/heroes-lists.component.ts
@@ -18,4 +18,4 @@ import {HeroService} from '../service/hero.service';
 export class HeroesListsComponent implements OnInit {
   heroes: Hero[];
 
-  constructor(private _heroService: HeroService, priva
+  constructor(private _heroService: HeroService, private router: 
