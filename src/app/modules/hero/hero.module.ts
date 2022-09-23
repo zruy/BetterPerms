58892814676ae11301680dd1884b41d06fc08629
zruy <109708027+zruy@users.noreply.github.com>
2022-09-23 06:30:34 +0000

    prototype

--- src/app/modules/hero/hero.module.ts
+++ src/app/modules/hero/hero.module.ts
@@ -3,3 +3,13 @@
  * User: sofia
  * Date: 2018/1/11
  * Version: 1.0.0
+ * Description:
+ */
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {HeroService} from './service/hero.service';
+
+import {HeroesPageComponent} from './pages/heroes.page';
