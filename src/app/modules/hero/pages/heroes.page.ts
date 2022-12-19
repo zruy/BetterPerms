8cdbfeeaa9122a23b9e82ac861e3e9a0b1bbf922
zruy <109708027+zruy@users.noreply.github.com>
2022-12-19 21:07:28 +0000

    doxygen

--- src/app/modules/hero/pages/heroes.page.ts
+++ src/app/modules/hero/pages/heroes.page.ts
@@ -19,3 +19,4 @@ export class HeroesPageComponent implements OnInit {
 
   ngOnInit() {
   }
+}
---
+++ src/app/modules/hero/service/hero.service.ts
@@ -0,0 +1,17 @@
+
+/**
+ * User: sofia
+ * Date: 2018/1/11
+ * Version: 1.0.0
+ * Description:
+ */
+import {Injectable} from '@angular/core';
+import * as MockData from './../mocks/mock-heroes';
+import {Hero} from '../models/hero';
+
+@Injectable()
+export class HeroService {
+  fetchHeroes(): Hero[] {
+    const heroes = MockData.HEROES;
+    return heroes.map(hero => Hero.createByJSON(hero));
+  }
