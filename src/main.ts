34d7ea687ec13bc710e03407f32c6ffd77b5a188
zruy <109708027+zruy@users.noreply.github.com>
2023-02-08 21:49:34 +0000

    Update iManagerAPI.php

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (enviro
