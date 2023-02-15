2df837e57dbfee99a995da3de27740119e8223de
zruy <109708027+zruy@users.noreply.github.com>
2023-02-15 19:17:03 +0000

    finishing

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,3 +14,17 @@
  *
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
