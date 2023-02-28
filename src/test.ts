02480a0b88cc88cb5e9595d2a7a6c8a5cc9efc21
zruy <109708027+zruy@users.noreply.github.com>
2023-02-28 19:31:25 +0000

    update the doc

--- src/test.ts
+++ src/test.ts
@@ -28,3 +28,6 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
+context.keys().map(context);
+// Finally, start Karma to run the tests.
+__karma__.start();
